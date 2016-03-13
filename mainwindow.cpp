b4f1821b3d67fb8cda1e0e46c567dcbb2567ea39
Jo Pi <ferrdo@gmail.com>
2016-03-13 23:29:28 +0100

    added some logic

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,16 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->pushButton, SIGNAL(clicked(bool)), this, SLOT(clicked(bool)));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::clicked(bool)
+{
+    ui->pushButton_2->setVisible(!ui->pushButton_2->isVisible());
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,9 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+public slots:
+    void clicked(bool);
+
 private:
     Ui::MainWindow *ui;
 };
